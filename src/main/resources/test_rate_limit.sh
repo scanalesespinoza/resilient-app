94d18201623803bcd2f5274105fa242647cedba1
Sergio Canales <sergio.canales.e@gmail.com>
2024-04-19 15:10:37 -0400

    I remove incomplete login to stream recover event
    Will retake it in a feature branch

--- src/main/resources/test_rate_limit.sh
+++ src/main/resources/test_rate_limit.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 # Endpoint URL
-URL="http://localhost:8080/users/all"
+URL="http://localhost:8080/api/users/all"
 
 # Number of requests
-REQUESTS=60
+REQUESTS=20
 
 # Loop for the specified number of requests
 for (( i=1; i<=REQUESTS; i++ ))
 do
    echo "Request $i"
    curl -s -o /dev/null -w "%{http_code} - %{url_effective}\n" $URL
-   sleep 0.1  # Delay to simulate second-by-second requests
+   sleep 0.5  # Delay to simulate second-by-second requests
 done
